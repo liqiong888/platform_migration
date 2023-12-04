9e7867c437cd9541c3c8711fc8ca43476ea10420
LiQiong <15594952639@163.com>
2023-12-04 11:47:31 +0800

    init

---
+++ src/main/java/org/delivery/PlatformMigrationApplication.java
@@ -0,0 +1,13 @@
+package org.delivery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformMigrationApplication.class, args);
+    }
+}
